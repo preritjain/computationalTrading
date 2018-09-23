ddc8df8a29b8a82fcbc8755a2a03efcbf999fa53
preritjain93@gmail.com <preritjain93@gmail.com>
2018-09-23 11:50:21 +0530

    trying out the api

---
+++ src/apiSetup.py
@@ -0,0 +1,21 @@
+from binance.client import Client
+import json
+
+fp = open('key.json','r')
+keys = json.load(fp)
+
+client = Client(keys['api_key'], keys['secret_key'])
+# get market depth
+depth = client.get_order_book(symbol='BNBBTC')
+print(depth)
+
+
+# fetch list of withdrawals
+# withdraws = client.get_withdraw_history()
+# print(withdraws)
+# 
+# trades = client.get_my_trades(symbol='ETHUSDT')
+# print(trades)
+
+fees = client.get_trade_fee()
+print(fees)
